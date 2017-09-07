b3985988b3887690838ca9354ec028d2274be2bf
mrbeesley <mrbeesley@gmail.com>
2017-09-07 15:44:20 -0500

    missed a file

---
+++ RentalManagementModels/RentalManagementModels/Utility/USState.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace RentalManagementModels.Utility
+{
+    /// <summary>
+    /// Represents all of the states in the US
+    /// </summary>
+    public enum USState
+    {
+        /// <summary>
+        /// AL
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Alabama")]
+        AL,
+
+        /// <summary>
+        /// AK
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Alaska")]
+        AK,
+
+        /// <summary>
+        /// AR
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Arkansas")]
+        AR,
+
+        /// <summary>
+        /// AZ
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Arizona")]
+        AZ,
+
+        /// <summary>
+        /// CA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("California")]
+        CA,
+
+        /// <summary>
+        /// CO
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Colorado")]
+        CO,
+
+        /// <summary>
+        /// CT
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Connecticut")]
+        CT,
+
+        /// <summary>
+        /// DC
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("D.C.")]
+        DC,
+
+        /// <summary>
+        /// DE
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Delaware")]
+        DE,
+
+        /// <summary>
+        /// FL
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Florida")]
+        FL,
+
+        /// <summary>
+        /// GA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Georgia")]
+        GA,
+
+        /// <summary>
+        /// HI
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Hawaii")]
+        HI,
+
+        /// <summary>
+        /// IA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Iowa")]
+        IA,
+
+        /// <summary>
+        /// ID
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Idaho")]
+        ID,
+
+        /// <summary>
+        /// IL
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Illinois")]
+        IL,
+
+        /// <summary>
+        /// IN
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Indiana")]
+        IN,
+
+        /// <summary>
+        /// KS
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Kansas")]
+        KS,
+
+        /// <summary>
+        /// KY
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Kentucky")]
+        KY,
+
+        /// <summary>
+        /// LA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Louisiana")]
+        LA,
+
+        /// <summary>
+        /// MA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Massachusetts")]
+        MA,
+
+        /// <summary>
+        /// MD
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Maryland")]
+        MD,
+
+        /// <summary>
+        /// ME
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Maine")]
+        ME,
+
+        /// <summary>
+        /// MI
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Michigan")]
+        MI,
+
+        /// <summary>
+        /// MN
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Minnesota")]
+        MN,
+
+        /// <summary>
+        /// MO
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Missouri")]
+        MO,
+
+        /// <summary>
+        /// MS
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Mississippi")]
+        MS,
+
+        /// <summary>MT
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Montana")]
+        MT,
+
+        /// <summary>
+        /// NC
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("North Carolina")]
+        NC,
+
+        /// <summary>
+        /// ND
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("North Dakota")]
+        ND,
+
+        /// <summary>
+        /// NE
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Nebraska")]
+        NE,
+
+        /// <summary>
+        /// NH
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("New Hampshire")]
+        NH,
+
+        /// <summary>
+        /// NJ
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("New Jersey")]
+        NJ,
+
+        /// <summary>
+        /// NM
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("New Mexico")]
+        NM,
+
+        /// <summary>
+        /// NV
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Nevada")]
+        NV,
+
+        /// <summary>
+        /// NY
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("New York")]
+        NY,
+
+        /// <summary>
+        /// OK
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Oklahoma")]
+        OK,
+
+        /// <summary>
+        /// OH
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Ohio")]
+        OH,
+
+        /// <summary>
+        /// OR
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Oregon")]
+        OR,
+
+        /// <summary>
+        /// PA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Pennsylvania")]
+        PA,
+
+        /// <summary>
+        /// RI
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Rhode Island")]
+        RI,
+
+        /// <summary>
+        /// SC
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("South Carolina")]
+        SC,
+
+        /// <summary>
+        /// SD
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("South Dakota")]
+        SD,
+        
+        /// <summary>
+        /// TN
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Tennessee")]
+        TN,
+
+        /// <summary>
+        /// TX
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Texas")]
+        TX,
+
+        /// <summary>
+        /// UT
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Utah")]
+        UT,
+
+        /// <summary>
+        /// VA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Virginia")]
+        VA,
+
+        /// <summary>
+        /// VT
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Vermont")]
+        VT,
+
+        /// <summary>
+        /// WA
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Washington")]
+        WA,
+
+        /// <summary>
+        /// WI
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Wisconsin")]
+        WI,
+
+        /// <summary>
+        /// WV
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("West Virginia")]
+        WV,
+
+        /// <summary>
+        /// WY
+        /// State in the US check description for state name
+        /// </summary>
+        [Description("Wyoming")]
+        WY
+
+    }
+}
